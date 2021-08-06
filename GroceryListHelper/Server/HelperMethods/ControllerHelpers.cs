d0dc5ad9ce8ea3ec09530bcb46c9b80049f75057
Markus Nisula <markus_nisula@hotmail.com>
2021-08-07 01:11:22 +0300

    Auth settings test with signalr

--- GroceryListHelper/Server/HelperMethods/ControllerHelpers.cs
+++ GroceryListHelper/Server/HelperMethods/ControllerHelpers.cs
@@ -6,7 +6,7 @@ namespace GroceryListHelper.Server.HelperMethods
     {
         public static int GetUserId(this ClaimsPrincipal user)
         {
-            return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier).Value);
+            return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? user.FindFirst("Id")?.Value);
         }
     }
 }
--- GroceryListHelper/Server/Installers/AuthenticationInstaller.cs
+++ GroceryListHelper/Server/Installers/AuthenticationInstaller.cs
@@ -1,7 +1,10 @@
 ﻿using GroceryListHelper.Server.HelperMethods;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Primitives;
+using System.Threading.Tasks;
 
 namespace GroceryListHelper.Server.Installers
 {
@@ -20,6 +23,27 @@ namespace GroceryListHelper.Server.Installers
             {
                 x.SaveToken = true;
                 x.TokenValidationParameters = tokenValidationParametersFactory.CreateParameters("AccessTokenKey");
+                x.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        PathString path = context.HttpContext.Request.Path;
+
+                        if (path.StartsWithSegments("/carthub"))
+                        {
+                            string accessToken = context.Request.Query["access_token"];
+                            if(string.IsNullOrEmpty(accessToken) && context.Request.Headers.TryGetValue("Authorization", out StringValues token))
+                            {
+                                accessToken = token;
+                            }
+                            if (!string.IsNullOrEmpty(accessToken))
+                            {
+                                context.Token = accessToken.Replace("Bearer ", "");
+                            }
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
             });
             services.AddScoped<JWTAuthenticationManager>();
         }
