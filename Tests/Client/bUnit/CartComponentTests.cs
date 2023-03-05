f63d7fa474a7e977e99fb2dc53cd562e895dd241
Markus Nisula <markus_nisula@hotmail.com>
2023-03-05 09:28:51 +0200

    Fix CartComponent tests

--- Tests/Client/bUnit/CartComponentTests.cs
+++ Tests/Client/bUnit/CartComponentTests.cs
@@ -95,64 +95,15 @@ public class CartComponentTests : TestContext
         cartProductsServiceMock.GetCartProducts().Returns(products);
         storeProductsServiceMock.GetStoreProducts().Returns(new List<StoreProduct>());
         IRenderedComponent<CartComponent> cut = RenderComponent<CartComponent>();
-        cut.MarkupMatches($"""
-            <table b-l8ub2h82qp>
-                <thead b-l8ub2h82qp>
-                    <tr b-l8ub2h82qp>
-                        <th b-l8ub2h82qp>Reorder</th>
-                        <th b-l8ub2h82qp>Collected</th>
-                        <th class="product-col" blazor:onclick="1" style="cursor: pointer;" b-l8ub2h82qp>Product</th>
-                        <th class="number-col" b-l8ub2h82qp>Amount</th>
-                        <th class="number-col" b-l8ub2h82qp>Price</th>
-                        <th b-l8ub2h82qp>Total</th>
-                        <th b-l8ub2h82qp></th>
-                        <th b-l8ub2h82qp></th>
-                    </tr>
-                </thead>
-                <tbody b-l8ub2h82qp>
-                    <tr b-l8ub2h82qp>
-                        <td b-l8ub2h82qp></td>
-                        <td b-l8ub2h82qp><button id="add-cartproduct-button" type="submit" class="btn btn-success" blazor:onclick="2" aria-label="Add product" b-l8ub2h82qp>Add</button></td>
-                        <td b-l8ub2h82qp><input id="newproduct-name-input" type="text" list="products" class="form-control" aria-label="Product name input" autocomplete="off" blazor:onfocusout="3" value="" blazor:onchange="4" b-l8ub2h82qp blazor:elementReference="b61369d5-982f-4818-a1e7-08e42ecc42ae"></input>
-                            <datalist id="products" b-l8ub2h82qp></datalist>
-                        </td>
-                        <td b-l8ub2h82qp><input id="newproduct-amount-input" type="number" step="1" min="1" class="form-control text-center" aria-label="Product amount input" value="1" blazor:onchange="5" b-l8ub2h82qp /></td>
-                        <td b-l8ub2h82qp><input id="newproduct-price-input" type="number" step="0.01" min="0" class="form-control text-center" aria-label="Product unit price input" value="0" blazor:onchange="6" b-l8ub2h82qp /></td>
-                        <td b-l8ub2h82qp></td>
-                    </tr>
-                    <tr class="{(products[0].IsCollected ? "checked-item" : "")}" b-l8ub2h82qp>
-                        <td b-l8ub2h82qp><button id="reorder-button-0" class="btn btn-primary sort-button " title="Move" aria-label="Move" blazor:onclick="7" b-l8ub2h82qp><i class="fas fa-sort" b-l8ub2h82qp></i></button></td>
-                        <td b-l8ub2h82qp><input id="item-collected-checkbox-0" type="checkbox" class="checkbox" blazor:onchange="8" b-l8ub2h82qp {(products[0].IsCollected ? "checked" : "")}/></td>
-                        <td id="item-name-0" style="text-decoration: {(products[0].IsCollected ? "line-through" : "none")}" aria-label="Product name" b-l8ub2h82qp>{products[0].Name}</td>
-                        <td id="item-amount-0" aria-label="Amount" b-l8ub2h82qp>{products[0].Amount}</td>
-                        <td id="item-unitprice-0" aria-label="Unit price" b-l8ub2h82qp>{products[0].UnitPrice:N2}</td>
-                        <td id="item-totalprice-0" aria-label="Total price" b-l8ub2h82qp>{products[0].Total:N2}</td>
-                        <td b-l8ub2h82qp><button id="edit-product-button-0" class="btn btn-success" blazor:onclick="9" aria-label="Edit product" {(products[0].IsCollected ? "disabled" : "")} b-l8ub2h82qp><i class="fas fa-edit" b-l8ub2h82qp></i></button></td>
-                        <td b-l8ub2h82qp><button id="delete-product-button-0" class="btn btn-danger" blazor:onclick="10" aria-label="Delete product" {(products[0].IsCollected ? "disabled" : "")} b-l8ub2h82qp>X</button></td>
-                    </tr>
-                    <tr class="{(products[1].IsCollected ? "checked-item" : "")}" b-l8ub2h82qp>
-                        <td b-l8ub2h82qp><button id="reorder-button-1" class="btn btn-primary sort-button " title="Move" aria-label="Move" blazor:onclick="11" b-l8ub2h82qp><i class="fas fa-sort" b-l8ub2h82qp></i></button></td>
-                        <td b-l8ub2h82qp><input id="item-collected-checkbox-1" type="checkbox" class="checkbox" blazor:onchange="12" b-l8ub2h82qp {(products[1].IsCollected ? "checked" : "")}/></td>
-                        <td id="item-name-1" style="text-decoration: {(products[1].IsCollected ? "line-through" : "none")}" aria-label="Product name" b-l8ub2h82qp>{products[1].Name}</td>
-                        <td id="item-amount-1" aria-label="Amount" b-l8ub2h82qp>{products[1].Amount}</td>
-                        <td id="item-unitprice-1" aria-label="Unit price" b-l8ub2h82qp>{products[1].UnitPrice:N2}</td>
-                        <td id="item-totalprice-1" aria-label="Total price" b-l8ub2h82qp>{products[1].Total:N2}</td>
-                        <td b-l8ub2h82qp><button id="edit-product-button-1" class="btn btn-success" blazor:onclick="13" aria-label="Edit product" {(products[1].IsCollected ? "disabled" : "")} b-l8ub2h82qp><i class="fas fa-edit" b-l8ub2h82qp></i></button></td>
-                        <td b-l8ub2h82qp><button id="delete-product-button-1" class="btn btn-danger" blazor:onclick="14" aria-label="Delete product" {(products[1].IsCollected ? "disabled" : "")} b-l8ub2h82qp>X</button></td>
-                    </tr>
-                    <tr class="{(products[2].IsCollected ? "checked-item" : "")}" b-l8ub2h82qp>
-                        <td b-l8ub2h82qp><button id="reorder-button-2" class="btn btn-primary sort-button " title="Move" aria-label="Move" blazor:onclick="15" b-l8ub2h82qp><i class="fas fa-sort" b-l8ub2h82qp></i></button></td>
-                        <td b-l8ub2h82qp><input id="item-collected-checkbox-2" type="checkbox" class="checkbox" blazor:onchange="16" b-l8ub2h82qp {(products[2].IsCollected ? "checked" : "")}/></td>
-                        <td id="item-name-2" style="text-decoration: {(products[2].IsCollected ? "line-through" : "none")}" aria-label="Product name" b-l8ub2h82qp>{products[2].Name}</td>
-                        <td id="item-amount-2" aria-label="Amount" b-l8ub2h82qp>{products[2].Amount}</td>
-                        <td id="item-unitprice-2" aria-label="Unit price" b-l8ub2h82qp>{products[2].UnitPrice:N2}</td>
-                        <td id="item-totalprice-2" aria-label="Total price" b-l8ub2h82qp>{products[2].Total:N2}</td>
-                        <td b-l8ub2h82qp><button id="edit-product-button-2" class="btn btn-success" blazor:onclick="17" aria-label="Edit product" b-l8ub2h82qp {(products[2].IsCollected ? "disabled" : "")}><i class="fas fa-edit" b-l8ub2h82qp></i></button></td>
-                        <td b-l8ub2h82qp><button id="delete-product-button-2" class="btn btn-danger" blazor:onclick="18" aria-label="Delete product" b-l8ub2h82qp {(products[2].IsCollected ? "disabled" : "")}>X</button></td>
-                    </tr>
-                </tbody>
-            </table>
-            """);
+        foreach (var p in products)
+        {
+            Assert.NotNull(cut.Find($"td:contains(\"{p.Name}\")"));
+            Assert.NotNull(cut.Find($"td:contains(\"{p.Amount}\")"));
+            Assert.NotNull(cut.Find($"td:contains(\"{p.UnitPrice:N2}\")"));
+            Assert.NotNull(cut.Find($"td:contains(\"{p.Total:N2}\")"));
+        }
+        Assert.Equal(products.Count(x => x.IsCollected), cut.FindAll("input[checked]").Count);
+        Assert.Equal(products.Count(x => x.IsCollected), cut.FindAll("tr.checked-item").Count);
     }
 
     [Theory]
@@ -168,64 +119,15 @@ public class CartComponentTests : TestContext
         Assert.True(persisted);
         Assert.NotNull(persistedProducts);
         Assert.Equal(products.Count, persistedProducts.Count);
-        cut.MarkupMatches($"""
-            <table b-l8ub2h82qp>
-                <thead b-l8ub2h82qp>
-                    <tr b-l8ub2h82qp>
-                        <th b-l8ub2h82qp>Reorder</th>
-                        <th b-l8ub2h82qp>Collected</th>
-                        <th class="product-col" blazor:onclick="1" style="cursor: pointer;" b-l8ub2h82qp>Product</th>
-                        <th class="number-col" b-l8ub2h82qp>Amount</th>
-                        <th class="number-col" b-l8ub2h82qp>Price</th>
-                        <th b-l8ub2h82qp>Total</th>
-                        <th b-l8ub2h82qp></th>
-                        <th b-l8ub2h82qp></th>
-                    </tr>
-                </thead>
-                <tbody b-l8ub2h82qp>
-                    <tr b-l8ub2h82qp>
-                        <td b-l8ub2h82qp></td>
-                        <td b-l8ub2h82qp><button id="add-cartproduct-button" type="submit" class="btn btn-success" blazor:onclick="2" aria-label="Add product" b-l8ub2h82qp>Add</button></td>
-                        <td b-l8ub2h82qp><input id="newproduct-name-input" type="text" list="products" class="form-control" aria-label="Product name input" autocomplete="off" blazor:onfocusout="3" value="" blazor:onchange="4" b-l8ub2h82qp blazor:elementReference="b61369d5-982f-4818-a1e7-08e42ecc42ae"></input>
-                            <datalist id="products" b-l8ub2h82qp></datalist>
-                        </td>
-                        <td b-l8ub2h82qp><input id="newproduct-amount-input" type="number" step="1" min="1" class="form-control text-center" aria-label="Product amount input" value="1" blazor:onchange="5" b-l8ub2h82qp /></td>
-                        <td b-l8ub2h82qp><input id="newproduct-price-input" type="number" step="0.01" min="0" class="form-control text-center" aria-label="Product unit price input" value="0" blazor:onchange="6" b-l8ub2h82qp /></td>
-                        <td b-l8ub2h82qp></td>
-                    </tr>
-                    <tr class="{(products[0].IsCollected ? "checked-item" : "")}" b-l8ub2h82qp>
-                        <td b-l8ub2h82qp><button id="reorder-button-0" class="btn btn-primary sort-button " title="Move" aria-label="Move" blazor:onclick="7" b-l8ub2h82qp><i class="fas fa-sort" b-l8ub2h82qp></i></button></td>
-                        <td b-l8ub2h82qp><input id="item-collected-checkbox-0" type="checkbox" class="checkbox" blazor:onchange="8" b-l8ub2h82qp {(products[0].IsCollected ? "checked" : "")}/></td>
-                        <td id="item-name-0" style="text-decoration: {(products[0].IsCollected ? "line-through" : "none")}" aria-label="Product name" b-l8ub2h82qp>{products[0].Name}</td>
-                        <td id="item-amount-0" aria-label="Amount" b-l8ub2h82qp>{products[0].Amount}</td>
-                        <td id="item-unitprice-0" aria-label="Unit price" b-l8ub2h82qp>{products[0].UnitPrice:N2}</td>
-                        <td id="item-totalprice-0" aria-label="Total price" b-l8ub2h82qp>{products[0].Total:N2}</td>
-                        <td b-l8ub2h82qp><button id="edit-product-button-0" class="btn btn-success" blazor:onclick="9" aria-label="Edit product" {(products[0].IsCollected ? "disabled" : "")} b-l8ub2h82qp><i class="fas fa-edit" b-l8ub2h82qp></i></button></td>
-                        <td b-l8ub2h82qp><button id="delete-product-button-0" class="btn btn-danger" blazor:onclick="10" aria-label="Delete product" {(products[0].IsCollected ? "disabled" : "")} b-l8ub2h82qp>X</button></td>
-                    </tr>
-                    <tr class="{(products[1].IsCollected ? "checked-item" : "")}" b-l8ub2h82qp>
-                        <td b-l8ub2h82qp><button id="reorder-button-1" class="btn btn-primary sort-button " title="Move" aria-label="Move" blazor:onclick="11" b-l8ub2h82qp><i class="fas fa-sort" b-l8ub2h82qp></i></button></td>
-                        <td b-l8ub2h82qp><input id="item-collected-checkbox-1" type="checkbox" class="checkbox" blazor:onchange="12" b-l8ub2h82qp {(products[1].IsCollected ? "checked" : "")}/></td>
-                        <td id="item-name-1" style="text-decoration: {(products[1].IsCollected ? "line-through" : "none")}" aria-label="Product name" b-l8ub2h82qp>{products[1].Name}</td>
-                        <td id="item-amount-1" aria-label="Amount" b-l8ub2h82qp>{products[1].Amount}</td>
-                        <td id="item-unitprice-1" aria-label="Unit price" b-l8ub2h82qp>{products[1].UnitPrice:N2}</td>
-                        <td id="item-totalprice-1" aria-label="Total price" b-l8ub2h82qp>{products[1].Total:N2}</td>
-                        <td b-l8ub2h82qp><button id="edit-product-button-1" class="btn btn-success" blazor:onclick="13" aria-label="Edit product" {(products[1].IsCollected ? "disabled" : "")} b-l8ub2h82qp><i class="fas fa-edit" b-l8ub2h82qp></i></button></td>
-                        <td b-l8ub2h82qp><button id="delete-product-button-1" class="btn btn-danger" blazor:onclick="14" aria-label="Delete product" {(products[1].IsCollected ? "disabled" : "")} b-l8ub2h82qp>X</button></td>
-                    </tr>
-                    <tr class="{(products[2].IsCollected ? "checked-item" : "")}" b-l8ub2h82qp>
-                        <td b-l8ub2h82qp><button id="reorder-button-2" class="btn btn-primary sort-button " title="Move" aria-label="Move" blazor:onclick="15" b-l8ub2h82qp><i class="fas fa-sort" b-l8ub2h82qp></i></button></td>
-                        <td b-l8ub2h82qp><input id="item-collected-checkbox-2" type="checkbox" class="checkbox" blazor:onchange="16" b-l8ub2h82qp {(products[2].IsCollected ? "checked" : "")}/></td>
-                        <td id="item-name-2" style="text-decoration: {(products[2].IsCollected ? "line-through" : "none")}" aria-label="Product name" b-l8ub2h82qp>{products[2].Name}</td>
-                        <td id="item-amount-2" aria-label="Amount" b-l8ub2h82qp>{products[2].Amount}</td>
-                        <td id="item-unitprice-2" aria-label="Unit price" b-l8ub2h82qp>{products[2].UnitPrice:N2}</td>
-                        <td id="item-totalprice-2" aria-label="Total price" b-l8ub2h82qp>{products[2].Total:N2}</td>
-                        <td b-l8ub2h82qp><button id="edit-product-button-2" class="btn btn-success" blazor:onclick="17" aria-label="Edit product" b-l8ub2h82qp {(products[2].IsCollected ? "disabled" : "")}><i class="fas fa-edit" b-l8ub2h82qp></i></button></td>
-                        <td b-l8ub2h82qp><button id="delete-product-button-2" class="btn btn-danger" blazor:onclick="18" aria-label="Delete product" b-l8ub2h82qp {(products[2].IsCollected ? "disabled" : "")}>X</button></td>
-                    </tr>
-                </tbody>
-            </table>
-            """);
+        foreach (var p in products)
+        {
+            Assert.NotNull(cut.Find($"td:contains(\"{p.Name}\")"));
+            Assert.NotNull(cut.Find($"td:contains(\"{p.Amount}\")"));
+            Assert.NotNull(cut.Find($"td:contains(\"{p.UnitPrice:N2}\")"));
+            Assert.NotNull(cut.Find($"td:contains(\"{p.Total:N2}\")"));
+        }
+        Assert.Equal(products.Count(x=>x.IsCollected), cut.FindAll("input[checked]").Count);
+        Assert.Equal(products.Count(x=>x.IsCollected), cut.FindAll("tr.checked-item").Count);
     }
 
     [Fact]
