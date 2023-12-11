c4659ee7b0b6df166a85e3faa8e00bf1c164eab5
Markus Nisula <markus_nisula@hotmail.com>
2023-12-11 16:54:44 +0200

    Set E2E tests to headless

--- GroceryListHelper.E2ETests/Infrastructure/WebApplicationFactoryFixtures.cs
+++ GroceryListHelper.E2ETests/Infrastructure/WebApplicationFactoryFixtures.cs
@@ -32,7 +32,7 @@ public sealed class WebApplicationFactoryFixture : WebApplicationFactory<Grocery
         PlaywrightInstance = await Playwright.CreateAsync();
         BrowserInstance = await PlaywrightInstance.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
         {
-            Headless = false,
+            Headless = true,
             SlowMo = 200,
         });
     }
