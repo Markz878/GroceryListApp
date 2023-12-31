9bc33d2ffa392bf7ec11b13eadff14f3798222cd
Markus Nisula <markus_nisula@hotmail.com>
2023-12-31 18:27:14 +0200

    Remove AntiforgeryTokenFilter

--- GroceryListHelper.Server/Endpoints/APIEndpointMapper.cs
+++ GroceryListHelper.Server/Endpoints/APIEndpointMapper.cs
@@ -7,10 +7,6 @@ public static class APIEndpointMapper
     public static void MapAPIEndpoints(this WebApplication app)
     {
         RouteGroupBuilder apiGroup = app.MapGroup("api").RequireAuthorization();
-        if (app.Environment.IsProduction())
-        {
-            apiGroup.AddEndpointFilter<AntiforgeryTokenFilter>();
-        }
         apiGroup.AddFluentValidation();
         apiGroup.RequireRateLimiting(RateLimitInstaller.PolicyName);
         apiGroup.AddAccountEndpoints();
