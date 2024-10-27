3ef2b88ffdeea5d0e04bce89fb918065099a5311
Markus Nisula <markus_nisula@hotmail.com>
2024-10-27 11:53:03 +0200

    Remove vite config https setup

--- GroceryListHelper.Client/vite.config.ts
+++ GroceryListHelper.Client/vite.config.ts
@@ -1,40 +1,8 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { fileURLToPath, URL } from 'node:url';
-import { join } from 'path';
-import { readFileSync, existsSync } from 'fs'
-import { spawnSync } from 'child_process';
 import { checker } from "vite-plugin-checker"
 
-const baseFolder =
-  process.env.APPDATA !== undefined && process.env.APPDATA !== ''
-    ? `${process.env.APPDATA}/ASP.NET/https`
-    : `${process.env.HOME}/.aspnet/https`;
-
-const certificateArg = process.argv.map((arg: string) => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0];
-const certificateName = certificateArg ? certificateArg.groups.value : "vueapp1.client";
-
-if (!certificateName) {
-  console.error('Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.')
-  process.exit(-1);
-}
-
-const certFilePath = join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = join(baseFolder, `${certificateName}.key`);
-
-if (!existsSync(certFilePath) || !existsSync(keyFilePath)) {
-  if (0 !== spawnSync('dotnet', [
-    'dev-certs',
-    'https',
-    '--export-path',
-    certFilePath,
-    '--format',
-    'Pem',
-    '--no-password',
-  ], { stdio: 'inherit', }).status) {
-    throw new Error("Could not create certificate.");
-  }
-}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), checker({ typescript: true })],
@@ -50,21 +18,17 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://localhost:7021/',
+        target: 'http://localhost:7021/',
         changeOrigin: true,
         secure: false
       },
       '/signin-oidc': {
-        target: 'https://localhost:7021/',
+        target: 'http://localhost:7021/',
         changeOrigin: true,
         secure: false
       }
     },
     port: 5173,
-    https: {
-      key: readFileSync(keyFilePath),
-      cert: readFileSync(certFilePath),
-    }
   }
 })
 
