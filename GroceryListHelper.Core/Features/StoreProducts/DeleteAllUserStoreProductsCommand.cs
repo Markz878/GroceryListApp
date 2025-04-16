5572f1826ed02c31c4da367a198e60aa99de7532
Markz878 <markus_nisula@hotmail.com>
2025-04-16 15:07:03 +0300

    Test container.ReplaceContainerAsync

--- GroceryListHelper.Core/Features/StoreProducts/DeleteAllUserStoreProductsCommand.cs
+++ GroceryListHelper.Core/Features/StoreProducts/DeleteAllUserStoreProductsCommand.cs
@@ -9,7 +9,14 @@ internal sealed class DeleteAllUserStoreProductsCommandHandler(CosmosClient db) 
 {
     public async Task Handle(DeleteAllUserStoreProductsCommand request, CancellationToken cancellationToken = default)
     {
+
         Container container = db.GetContainer(DataAccessConstants.Database, DataAccessConstants.StoreProductsContainer);
+
+        ContainerResponse containerResponse = await container.ReadContainerAsync();
+        ContainerProperties containerProperties = containerResponse.Resource;
+        containerProperties.DefaultTimeToLive = int.MaxValue;
+        await container.ReplaceContainerAsync(containerProperties);
+
         string sql = "SELECT * FROM c WHERE c.userId=@userId";
         QueryDefinition query = new QueryDefinition(sql)
             .WithParameter("@userId", request.UserId);
